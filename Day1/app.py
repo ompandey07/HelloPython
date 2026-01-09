66f152fd9c8b446ddb9b02f61ad4dd51a0251801
ompandey07 <iamompadney.it@gmail.com>
2026-01-09 17:52:29 +0545

    aded a day one basics

---
+++ Day1/app.py
@@ -0,0 +1,13 @@
+# this is how you can print something in Python 
+# print("Hello  World!")
+
+
+# varaiables and realtions in python
+# fruit = "Apple"
+# vegitable = "Carrot"
+# print(fruit + "      " +  vegitable)
+
+
+# num1 = 10
+# num2 = 20
+# print("The sum of num1 and num2 "  , num1 + num2)
