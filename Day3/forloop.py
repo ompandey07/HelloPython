19855d908e2c89a958e743174dee3d3e96ec62ee
ompandey07 <iamompadney.it@gmail.com>
2026-01-11 18:17:23 +0545

    Day 3 Compleated

---
+++ Day3/forloop.py
@@ -0,0 +1,19 @@
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits)
+# for x in fruits:
+#   print(x)
+
+
+# for a in range(10):
+#     print("I Love you")
+
+
+
+UserData = [
+    {"name": "Alice", "age": 30},
+    {"name": "Bob", "age": 25},
+    {"name": "Charlie", "age": 35}
+]
+
+for person in UserData:
+    print(person)
---
+++ Day3/gamewithwhileloop.py
@@ -0,0 +1,17 @@
+import random
+
+
+while True:
+
+    Name = input("Enter your name: ")
+    print(f"Welcome, {Name}!")
+
+    computer_number = random.randint(1, 10)
+
+    user_guess = int(input("Guess a number between 1 and 10: "))
+
+    if user_guess == computer_number:
+        print("Congratulations! You guessed the correct number.")
+        break
+    else:
+        print(f"Sorry, you guessed {user_guess}. The correct number was {computer_number}.")
