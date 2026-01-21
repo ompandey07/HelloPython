da1085764e8fa41438998c116cef31f6ece9b837
ompandey07 <iamompadney.it@gmail.com>
2026-01-21 18:39:35 +0545

    Updated Readme

--- Day5/app.py
+++ Day5/app.py
@@ -1,19 +1,8 @@
 # ANYTHING THAT IMPORTED TO USE IN CODE IS CALLED A MODULE , PACKAGE IS A COLLECTION OF MODULES
 
-from rembg import remove
-from PIL import Image
-import io
 
-# Open the original image
-input_path = "input_image.png"
-output_path = "output_image.png"
+# Simple Third Party Module Example
+# import pyjokes
+# print(pyjokes.get_joke())
 
-with open(input_path, "rb") as i:
-    input_data = i.read()
 
-# Remove background
-output_data = remove(input_data)
-
-# Save the result
-output_image = Image.open(io.BytesIO(output_data))
-output_image.save(output_path)
---
+++ Day5/project.py
@@ -0,0 +1,9 @@
+# import calendar
+
+# calander = calendar.calendar(2026)
+# print(calander)
+
+
+# import time
+# current_time = time.asctime()
+# print("Current Time:", current_time)
